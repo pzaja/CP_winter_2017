0d790754db486f049002067dce6748abcd8841a3
pzaja <p.zajaczkowska@gmail.com>
2017-10-26 13:55:28 +0200

    EmailCheck

---
+++ src/pl/waw/sgh/EmailCheck.java
@@ -0,0 +1,13 @@
+package pl.waw.sgh;
+
+public class EmailCheck {
+    public static void main(String[] args) {
+        final String EMAIL_OK = "user@sgh.pl";
+        final String EMAIL_NOT_OK1 = "@sgh.pl";
+
+
+        String email = EMAIL_NOT_OK1;
+
+        
+    }
+}
